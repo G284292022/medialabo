00e27ec77e65efb3ec6e5c5681d0eeb79d8d7215
228429 <228429@st.takushoku-u.ac.jp>
2023-05-21 21:43:50 +0900

    p

--- kazuate.js
+++ kazuate.js
@@ -3,7 +3,7 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 1;
+let kaisu = 2;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
@@ -16,13 +16,19 @@ hantei();
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let yoso = 9;
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
   for(; kaisu <= 3; kaisu = kaisu + 1){
     console.log(kaisu + "回目の予想: " + yoso);
+    let x = document.querySelector('span#kaisu');
+    x.textContent = kaisu;
+    let y = document.querySelector('span#answer');
+    y.textContent = yoso;
+    
+    // x.insertAdjacentElement('afterend',p#kaisu);
     if(kaisu === 3) continue;
   if(yoso === kotae){
     console.log("正解です．おめでとう!");
@@ -36,7 +42,7 @@ if(yoso === kotae){
   console.log("正解です．おめでとう!");
 }
 else {
-  console.log("まちがい．残念でした答えは "+kotae+" です．");
+  console.log("まちがい．残念でした答えは "+ kotae +" です．");
 }
   if(kaisu === 4){
   for(; kaisu <= 7; kaisu = kaisu + 1){
