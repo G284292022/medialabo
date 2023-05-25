863546f7ef1b926b4808860ec034e88e1e94a109
228429 <228429@st.takushoku-u.ac.jp>
2023-05-25 23:23:29 +0900

    p

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-let data = {
+let Beijing = {
   "coord": {
     "lon": 116.3972,
     "lat": 39.9075
@@ -44,11 +44,43 @@ let data = {
   "name": "北京市",
   "cod": 200
 };
+let London =
+{"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":803,"main":"Clouds","description":"曇りがち","icon":"04n"}],"base":"stations","main":{"temp":3.63,"feels_like":0.43,"temp_min":2.7,"temp_max":4.59,"pressure":1027,"humidity":79},"visibility":10000,"wind":{"speed":3.6,"deg":50},"clouds":{"all":75},"dt":1646542356,"sys":{"type":2,"id":2019646,"country":"GB","sunrise":1646548519,"sunset":1646588915},"timezone":0,"id":2643743,"name":"ロンドン","cod":200}
+
+let Cairo =
+{"coord":{"lon":31.2497,"lat":30.0626},"weather":[{"id":800,"main":"Clear","description":"晴天","icon":"01d"}],"base":"stations","main":{"temp":18.36,"feels_like":17.14,"temp_min":18.36,"temp_max":19.12,"pressure":990,"humidity":34},"visibility":10000,"wind":{"speed":0,"deg":0},"clouds":{"all":0},"dt":1646542151,"sys":{"type":2,"id":2046660,"country":"EG","sunrise":1646540154,"sunset":1646582223},"timezone":7200,"id":360630,"name":"カイロ","cod":200}
+
+let Moscow =
+{"coord":{"lon":37.6156,"lat":55.7522},"weather":[{"id":804,"main":"Clouds","description":"厚い雲","icon":"04d"}],"base":"stations","main":{"temp":-7.67,"feels_like":-12.36,"temp_min":-9.19,"temp_max":-5.98,"pressure":1018,"humidity":74,"sea_level":1018,"grnd_level":999},"visibility":10000,"wind":{"speed":2.7,"deg":272,"gust":8.41},"clouds":{"all":91},"dt":1646542400,"sys":{"type":2,"id":2018597,"country":"RU","sunrise":1646539734,"sunset":1646579588},"timezone":10800,"id":524901,"name":"モスクワ","cod":200}
+
+let Johannesburg =
+{"coord":{"lon":28.0436,"lat":-26.2023},"weather":[{"id":802,"main":"Clouds","description":"雲","icon":"03d"}],"base":"stations","main":{"temp":13.6,"feels_like":13.42,"temp_min":13.19,"temp_max":14.34,"pressure":1014,"humidity":92},"visibility":10000,"wind":{"speed":1.34,"deg":11,"gust":2.24},"clouds":{"all":30},"dt":1646542238,"sys":{"type":2,"id":2005686,"country":"ZA","sunrise":1646539457,"sunset":1646584459},"timezone":7200,"id":993800,"name":"ヨハネスブルグ","cod":200}
+
+let Tokyo = 
+{"coord":{"lon":139.6917,"lat":35.6895},"weather":[{"id":803,"main":"Clouds","description":"曇りがち","icon":"04d"}],"base":"stations","main":{"temp":11.8,"feels_like":9.58,"temp_min":9.25,"temp_max":13.77,"pressure":1004,"humidity":21},"visibility":10000,"wind":{"speed":6.17,"deg":130},"clouds":{"all":75},"dt":1646542321,"sys":{"type":2,"id":268395,"country":"JP","sunrise":1646514330,"sunset":1646556004},"timezone":32400,"id":1850147,"name":"東京都","cod":200}
+
+let Singapore =
+{"coord":{"lon":103.8501,"lat":1.2897},"weather":[{"id":803,"main":"Clouds","description":"曇りがち","icon":"04d"}],"base":"stations","main":{"temp":27.12,"feels_like":30.21,"temp_min":26.62,"temp_max":28.02,"pressure":1010,"humidity":82},"visibility":10000,"wind":{"speed":2.57,"deg":60},"clouds":{"all":75},"dt":1646542304,"sys":{"type":1,"id":9470,"country":"SG","sunrise":1646521998,"sunset":1646565536},"timezone":28800,"id":1880252,"name":"シンガポール","cod":200}
+
+let Sydney =
+{"coord":{"lon":151.2073,"lat":-33.8679},"weather":[{"id":500,"main":"Rain","description":"小雨","icon":"10d"}],"base":"stations","main":{"temp":22.42,"feels_like":23.15,"temp_min":21.31,"temp_max":24.23,"pressure":1004,"humidity":93},"visibility":10000,"wind":{"speed":2.24,"deg":179,"gust":7.6},"rain":{"1h":0.87},"clouds":{"all":98},"dt":1646542177,"sys":{"type":2,"id":2018875,"country":"AU","sunrise":1646509616,"sunset":1646555191},"timezone":39600,"id":2147714,"name":"シドニー","cod":200}
+
+let  Paris =
+{"coord":{"lon":2.3486,"lat":48.8534},"weather":[{"id":701,"main":"Mist","description":"霧","icon":"50n"}],"base":"stations","main":{"temp":2.05,"feels_like":-2.15,"temp_min":0.85,"temp_max":2.86,"pressure":1021,"humidity":89},"visibility":4900,"wind":{"speed":4.63,"deg":10},"clouds":{"all":20},"dt":1646542478,"sys":{"type":2,"id":2041230,"country":"FR","sunrise":1646547788,"sunset":1646588459},"timezone":3600,"id":2968815,"name":"Paris","cod":200}
+
+let RiodeJaneiro =
+{"coord":{"lon":-42.5,"lat":-22},"weather":[{"id":804,"main":"Clouds","description":"厚い雲","icon":"04n"}],"base":"stations","main":{"temp":18.03,"feels_like":18.4,"temp_min":18.03,"temp_max":18.03,"pressure":1015,"humidity":96,"sea_level":1015,"grnd_level":945},"visibility":10000,"wind":{"speed":1.21,"deg":166,"gust":1.1},"clouds":{"all":100},"dt":1646542479,"sys":{"country":"BR","sunrise":1646556521,"sunset":1646601250},"timezone":-10800,"id":3451189,"name":"State of Rio de Janeiro","cod":200}
+
+let NewYork =
+{"coord":{"lon":-74.006,"lat":40.7143},"weather":[{"id":804,"main":"Clouds","description":"厚い雲","icon":"04n"}],"base":"stations","main":{"temp":6.07,"feels_like":4.11,"temp_min":3.25,"temp_max":7.75,"pressure":1026,"humidity":71},"visibility":10000,"wind":{"speed":2.57,"deg":100},"clouds":{"all":100},"dt":1646542195,"sys":{"type":2,"id":2039034,"country":"US","sunrise":1646479438,"sunset":1646520681},"timezone":-18000,"id":5128581,"name":"ニューヨーク","cod":200}
+
+let LosAngeles =
+{"coord":{"lon":-118.2437,"lat":34.0522},"weather":[{"id":802,"main":"Clouds","description":"雲","icon":"03n"}],"base":"stations","main":{"temp":10.16,"feels_like":9.19,"temp_min":7.42,"temp_max":11.95,"pressure":1018,"humidity":75},"visibility":10000,"wind":{"speed":1.54,"deg":280},"clouds":{"all":40},"dt":1646542427,"sys":{"type":1,"id":3694,"country":"US","sunrise":1646489801,"sunset":1646531549},"timezone":-28800,"id":5368361,"name":"ロサンゼルス","cod":200}
 
 ////////// 課題3-2 ここからプログラムを書こう
 console.log(data);
 
-data = {
+Beijing = {
   name: "北京市",
   temp_min: 9.94,
   temp_max: 9.94
