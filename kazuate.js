5414d06a16ea43e15cc9b89dfd784e5ac6f0a5d0
228429 <228429@st.takushoku-u.ac.jp>
2023-05-25 17:31:15 +0900

    p

--- kazuate.js
+++ kazuate.js
@@ -16,60 +16,39 @@ b.addEventListener('click', hantei);
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let i = document.querySelector('input[name="seisu"]');
-  let a = Integer.parseInt(i.value);
-  let yoso = i.value; 
+  let a = Number(i.value);
+  let yoso = a; 
+
+  let x = document.querySelector('span#kaisu');;
+  x.textContent = kaisu;
+  let y = document.querySelector('span#answer');
+  y.textContent = yoso;
+  let z = document.querySelector('p#result');
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
 
-  for(; kaisu <= 1; kaisu = kaisu + 1){
-    console.log(kaisu + "回目の予想: " + yoso);
+  if(3>=kaisu){
 
-    let x = document.querySelector('span#kaisu');
-    let p2 = document.createElement('p');
-    p2.textContent = kaisu;
-    //x.insertAdjacentElement('afterend', p);
-    let y = document.querySelector('span#answer');
-    let p3 = document.createElement('p');
-    p3.textContent = yoso;
-    //y.insertAdjacentElement('afterend', p);
-    
-    if(kaisu === 3) continue;
-  if(yoso === kotae){
-    console.log("正解です．おめでとう!");
-    let z = document.querySelector('p#result');
-    let p = document.createElement('p');
-    p.textContent = '正解です．おめでとう!';
-    z.insertAdjacentElement('afterend', p);
+  if(kaisu === 3 &&  yoso !== kotae){
+    z.textContent="まちがい．残念でした答えは "+ kotae +" です．";
+  }else if(yoso === kotae){
+     
+     z.textContent = '正解です．おめでとう!';
+ 
+   } else if(yoso < kotae){
+     z.textContent = 'まちがい．答えはもっと大きいですよ';
+   } else if(yoso > kotae){
+     z.textContent = 'まちがい．答えはもっと小さいですよ';
+   }
+   }
+  else{
+    z.textContent="答えは "+kotae+" でした．すでにゲームは終わっています";
 
-  } else if(yoso < kotae){
-    console.log("まちがい．答えはもっと大きいですよ");
-    let z1 = document.querySelector('p#result');
-    let p = document.createElement('p');
-    p.textContent = 'まちがい．答えはもっと大きいですよ';
-    z1.insertAdjacentElement('afterend', p);
-  } else if(yoso > kotae){
-    console.log("まちがい．答えはもっと小さいですよ");
-    let z2 = document.querySelector('p#result');
-    let p = document.createElement('p');
-    p.textContent = 'まちがい．答えはもっと小さいですよ';
-    z2.insertAdjacentElement('afterend', p);
   }
-}
-if(yoso === kotae){
-  console.log("正解です．おめでとう!");
-}
-else {
-  console.log("まちがい．残念でした答えは "+ kotae +" です．");
 
-}
-  if(kaisu === 4){
-  for(; kaisu <= 7; kaisu = kaisu + 1){
-    console.log(kaisu + "回目の予想: " + yoso);
-    console.log("答えは "+kotae+" でした．すでにゲームは終わっています");
-  }
-}
+kaisu=kaisu+1;
 }
 
 let p1 = document.querySelector('p#null'); 
